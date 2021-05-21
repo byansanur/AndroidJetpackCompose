2828235be42e0459c7b89449bdef86cd3877bad0
Ratbyansa nur <ratbyansanur81@gmail.com>
2021-05-22 01:01:42 +0700

    optimize import

--- app/src/main/java/com/example/composeexample2/MainActivity.kt
+++ app/src/main/java/com/example/composeexample2/MainActivity.kt
@@ -12,8 +12,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.*
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
